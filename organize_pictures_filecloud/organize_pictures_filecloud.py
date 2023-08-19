0687e66ef720de738c719d939d76e56dd87d3459
Franklin Reyes <reyes852jr@gmail.com>
2023-08-19 16:06:12 -0400

    Added funtionality to extract date from filename regardless of the position of the date in the name. However, the format remains YYYYMMDD

--- organize_pictures_filecloud/organize_pictures_filecloud.py
+++ organize_pictures_filecloud/organize_pictures_filecloud.py
@@ -6,7 +6,7 @@ import sys
 import logging
 import time
 import xml.etree.ElementTree as ET
-
+import re
 
 def get_parameters():
     parser = argparse.ArgumentParser(description="Purpose: Organize files by year and month on FileCloud")
@@ -68,6 +68,17 @@ def search_files(cookies, source_folder, server_url):
 
     return files
 
+def extract_date_from_name(file_name):
+    """Extract the date from the file name"""
+    date = re.search(r'\d{8}', file_name).group(0)
+
+    """Check if the date is in the format YYYYMMDD"""
+    if date is not None and date.startswith("20") and len(date) == 8:
+        return date
+    else:
+        raise Exception("Date is not in the format YYYYMMDD")
+
+
 """Move files to the target folder, if it doesnt exists, create it"""""
 def move_files(cookies, files, target_folder, server_url):
 
@@ -89,103 +100,108 @@ def move_files(cookies, files, target_folder, server_url):
     }
 
     for file in files:
-        """Get the file name"""
-        file_name = file.split("/")[-1]
-        """Get the file's year and month"""
-        file_year = file_name[0:4]
-        file_month = month_map[file_name[4:6]]
+        try:
+            """Get the file name"""
+            file_name = file.split("/")[-1]
+            file_name_date = extract_date_from_name(file_name)
+            
+            """Get the file's year and month"""
+            file_year = file_name_date[0:4]
+            file_month = month_map[file_name_date[4:6]]
 
-        """Create the year folder if it exists"""
-        logger.info(f"> Checking if the '{target_folder}{file_year}' folder exists")
-        check_year_folder_exists = server_url + "/core/fileexists"
-        search_data = {
-            'file' : target_folder + file_year,
-            'caseinsensitive' : 1
-        }
+            """Create the year folder if it exists"""
+            logger.info(f"> Checking if the '{target_folder}{file_year}' folder exists")
+            check_year_folder_exists = server_url + "/core/fileexists"
+            search_data = {
+                'file' : target_folder + file_year,
+                'caseinsensitive' : 1
+            }
 
-        search_response = requests.post(check_year_folder_exists, data=search_data, cookies=cookies)
-        xml_data = ET.fromstring(search_response.text)
-        result_element = xml_data.find('.//result')
+            search_response = requests.post(check_year_folder_exists, data=search_data, cookies=cookies)
+            xml_data = ET.fromstring(search_response.text)
+            result_element = xml_data.find('.//result')
 
-        if result_element is not None:
-            if result_element.text == "0":
-                logger.info(f"> {target_folder}{file_year} folder does not exist. Creating it now.")
-                create_folder(cookies, target_folder + file_year, server_url)
-            elif result_element.text == "1":
-                logger.info(f"> {target_folder}{file_year} folder exists.")
-        else:
-            print("No 'result' element found in the response.") 
+            if result_element is not None:
+                if result_element.text == "0":
+                    logger.info(f"> {target_folder}{file_year} folder does not exist. Creating it now.")
+                    create_folder(cookies, target_folder + file_year, server_url)
+                elif result_element.text == "1":
+                    logger.info(f"> {target_folder}{file_year} folder exists.")
+            else:
+                print("No 'result' element found in the response.") 
 
-        """Checking if the month folder exists"""
-        logger.info(f"> Checking if the '{target_folder}{file_year}/{file_month}' folder exists")
+            """Checking if the month folder exists"""
+            logger.info(f"> Checking if the '{target_folder}{file_year}/{file_month}' folder exists")
 
-        check_month_folder_exists = server_url + "/core/fileexists"
-        search_data = {
-            'file' : target_folder + file_year + "/" + file_month,
-            'caseinsensitive' : 1
-        }
+            check_month_folder_exists = server_url + "/core/fileexists"
+            search_data = {
+                'file' : target_folder + file_year + "/" + file_month,
+                'caseinsensitive' : 1
+            }
 
-        search_response = requests.post(check_month_folder_exists, data=search_data, cookies=cookies)
-        xml_data = ET.fromstring(search_response.text)
-        result_element = xml_data.find('.//result')
+            search_response = requests.post(check_month_folder_exists, data=search_data, cookies=cookies)
+            xml_data = ET.fromstring(search_response.text)
+            result_element = xml_data.find('.//result')
 
-        if result_element is not None:
-            if result_element.text == "0":
-                logger.info(f"> {target_folder}{file_year}/{file_month} folder does not exist. Creating it now.")
-                create_folder(cookies, target_folder + file_year + "/" + file_month, server_url)
-            elif result_element.text == "1":
-                logger.info(f"> {target_folder}{file_year}/{file_month} folder exists.")
+            if result_element is not None:
+                if result_element.text == "0":
+                    logger.info(f"> {target_folder}{file_year}/{file_month} folder does not exist. Creating it now.")
+                    create_folder(cookies, target_folder + file_year + "/" + file_month, server_url)
+                elif result_element.text == "1":
+                    logger.info(f"> {target_folder}{file_year}/{file_month} folder exists.")
 
-        """Checking if file already exists"""
-        logger.info(f"> Checking if file exists in destination")
+            """Checking if file already exists"""
+            logger.info(f"> Checking if file exists in destination")
 
-        dest_file = target_folder + file_year + "/" + file_month + "/" + file_name
+            dest_file = target_folder + file_year + "/" + file_month + "/" + file_name
 
-        check_file_exists = server_url + "/core/fileexists"
-        search_data = {
-            'file' : dest_file,
-            'caseinsensitive' : 1
-        }
+            check_file_exists = server_url + "/core/fileexists"
+            search_data = {
+                'file' : dest_file,
+                'caseinsensitive' : 1
+            }
 
-        search_response = requests.post(check_file_exists, data=search_data, cookies=cookies)
-        xml_data = ET.fromstring(search_response.text)
-        result_element = xml_data.find('.//result')
+            search_response = requests.post(check_file_exists, data=search_data, cookies=cookies)
+            xml_data = ET.fromstring(search_response.text)
+            result_element = xml_data.find('.//result')
 
-        if result_element is not None:
-            if result_element.text == "0":
-                """Move the file to the target folder"""
-                logger.info(f"> Moving file '{file}' to '{target_folder}{file_year}/{file_month}'")
-                move_file_url = server_url + "/core/renameormove"
-                move_file_data = {
-                    'fromname' : file,
-                    'toname' : dest_file
-                }
+            if result_element is not None:
+                if result_element.text == "0":
+                    """Move the file to the target folder"""
+                    logger.info(f"> Moving file '{file}' to '{target_folder}{file_year}/{file_month}'")
+                    move_file_url = server_url + "/core/renameormove"
+                    move_file_data = {
+                        'fromname' : file,
+                        'toname' : dest_file
+                    }
 
-                move_file_response = requests.post(move_file_url, data=move_file_data, cookies=cookies)
+                    move_file_response = requests.post(move_file_url, data=move_file_data, cookies=cookies)
 
-                if move_file_response.status_code == 200:
-                    logger.info(f"> File '{file}' moved successfully!")
-                else:
-                    logger.info(f"> File '{file}' could not be moved")
-            elif result_element.text == "1":
-                logger.info(f"> {dest_file} exists. Deleting it from source")
+                    if move_file_response.status_code == 200:
+                        logger.info(f"> File '{file}' moved successfully!")
+                    else:
+                        logger.info(f"> File '{file}' could not be moved")
+                elif result_element.text == "1":
+                    logger.info(f"> {dest_file} exists. Deleting it from source")
 
-                path = file.rsplit('/', 1)[0]
-                file_name = file.split('/')[-1]
+                    path = file.rsplit('/', 1)[0]
+                    file_name = file.split('/')[-1]
 
-                """Delete the file from the source folder"""
-                delete_file_url = server_url + "/core/deletefile"
-                delete_file_data = {
-                    'path' : path,
-                    'name' : file_name
-                }
+                    """Delete the file from the source folder"""
+                    delete_file_url = server_url + "/core/deletefile"
+                    delete_file_data = {
+                        'path' : path,
+                        'name' : file_name
+                    }
 
-                delete_file_response = requests.post(delete_file_url, data=delete_file_data, cookies=cookies)
+                    delete_file_response = requests.post(delete_file_url, data=delete_file_data, cookies=cookies)
 
-                if delete_file_response.status_code == 200:
-                    logger.info(f"> File '{file}' deleted successfully!")
-                else:
-                    logger.info(f"> File '{file}' could not be deleted")
+                    if delete_file_response.status_code == 200:
+                        logger.info(f"> File '{file}' deleted successfully!")
+                    else:
+                        logger.info(f"> File '{file}' could not be deleted")
+        except Exception as e:
+            logger.error(f"> Exception: {e}")
 
 
 def create_folder(cookies, folder, server_url):
@@ -210,8 +226,6 @@ def create_folder(cookies, folder, server_url):
     except Exception as e:
         logger.info(f"> Exception: {e}")
 
-
-
 def main():
 
     args = get_parameters()
@@ -232,8 +246,7 @@ def main():
     if len(files) == 0:
         logger.info("No Files to move")
     else:
-        move_files(cookies, files, target_folder, server_url)
-        
+        move_files(cookies, files, target_folder, server_url)        
 
 if __name__ == "__main__":
     start_time = time.time()    
